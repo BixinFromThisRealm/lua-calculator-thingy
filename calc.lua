d7a5ac1ecd0ef329175dbc543b2a01d184334993
BixinFromThisRealm <12345678enosplayer@gmail.com>
2022-04-30 13:48:26 -0300

    made some comments and removed the license from the first comment

--- calc.lua
+++ calc.lua
@@ -20,33 +20,6 @@
     '.exit'        --> Breaks the main loop, terminating the application.
     '.cls'         --> Clears the terminal.
     '.help', '.h'  --> Prints this message.
-
-
-    BSD 2-Clause License
-
-    Copyright (c) 2022, BixinFromThisRealm
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this
-    list of conditions and the following disclaimer.
-
-    2. Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
-    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
 
 
@@ -117,8 +90,8 @@ OP = {
 }
 
 
-
---> Based on the Shunting-yard algorithm
+--> Creates a parse tree (in this case, a table) from a mathematical expression.
+--> Based on https://en.wikipedia.org/wiki/Shunting_yard_algorithm
 function parseIt(exp)
     exp = string.gsub(exp, '[%(%)%-%+%*/%%]', '  %0  ') --> ensure tokens are separated by spaces
 
@@ -180,7 +153,8 @@ function parseIt(exp)
 end
 
 
---> Based on https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#JavaScript
+--> Semantic parsing of the table genereated by parseIt(),
+--> this is where the actual interpretation and calculation happens.
 function analyzeIt(tokens)
     if tokens == nil then return end
 
